0d18f02d696eebcb470b8e6b35ff940436456aac
Tomek Ciołkowski <tomaszciolkowski@gmail.com>
2015-05-20 19:08:51 +0200

    Add comments to Producer

--- src/main/scala/system/items/Producer.scala
+++ src/main/scala/system/items/Producer.scala
@@ -8,7 +8,9 @@ trait Producer {
   /** Returns a new item which should be delivered onto a shelf. */
   def newItem: ItemType
 
+  /** Notifies the producer that some time has passed so it can update statistics. */
   def progress(dt: Double): Unit
 
+  /** Set probabilities for each item type to be generated. */
   def setProbabilities(p: Map[ItemType, Double])
 }
--- src/main/scala/system/items/ProducerImpl.scala
+++ src/main/scala/system/items/ProducerImpl.scala
@@ -7,11 +7,15 @@ import scala.util.Random
 class ProducerImpl extends Producer {
 
   private val random = new Random()
+
+  /** Map of item types and probabilities (values from range: [0..1]) that an item of given type will be generated. */
   private var itemProbabilities: Map[ItemType, Double]
     = Map(ItemType.Red -> 1/3.0, ItemType.Green -> 1/3.0, ItemType.Blue -> 1/3.0)
+
+  /** Item waiting to be picked up by BringerRobot. */
   private var currentItem: ItemType = randomItem()
 
-  /** Returns an item of a fixed type (for simplicity). */
+  /** Returns a random item generated using itemProbabilities. */
   override def newItem: ItemType = {
     val item = currentItem
     currentItem = randomItem()
